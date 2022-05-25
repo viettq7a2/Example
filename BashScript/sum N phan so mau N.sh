9bf6001dd015f2fb2f9535cd0d9a1827d77ab1be
viettq7a2 <106210146+viettq7a2@users.noreply.github.com>
2022-05-25 09:51:04 +0700

    Update sum N phan so mau N.sh

--- BashScript/sum N phan so mau N.sh
+++ BashScript/sum N phan so mau N.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
 clear
-echo -n "Vui long nhap so luong phan so muon tinh tong: "
-read N
-
+S=0
 tong () {
-for (( i=1 ; i <= N ; i++ ))
-do
-    S=`echo "$S + 1/$i" | bc -l`
-done
-echo -n "Tong cac phan so la: "
-printf "%.3f\n" $(echo "$S" | bc -l)
+    for (( i=1 ; i <= N ; i++ ))
+    do
+        S=`echo "$S + 1/$i" | bc -l`
+    done
+    echo -n "Tong cac phan so la: "
+    printf "%.3f\n" $(echo "$S" | bc -l)
 }
 
-S=0
-if [ $N -gt 0 ]
-then
+while true
+do
+    echo -n "Vui long nhap so luong phan so muon tinh tong: "
+    read N
+    if [ $N = "n" ] 
+    then
+        break
+    fi
     tong
-else   
-    while [ $N -le 0 ]
-    do
-        echo -n "Vui long nhap lai, so khong thoa man: "
+    if [ $N -le 0 ]
+    then
+        echo -n "So khong hop le, vui long nhap lai: "
         read N
-    done
-    tong
-fi
+    fi
+done
